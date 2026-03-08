49b1a07f6741124b98a705782979eb2c03b75ac5
chrkk <christianjake.alalim@cit.edu>
2026-03-08 14:16:59 +0800

    sim manager workes and main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
-public class Main{
-    public static void main(String[] args){
-        SimulationManager sim = new SimulationManager;
+import simulation.SimulationManager;   
+
+public class Main {
+    public static void main(String[] args) {
+        SimulationManager sim = new SimulationManager();
         sim.run();
     }
 }
