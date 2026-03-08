379d2e9207a84db3186db3752c5970af4d3d5a60
Ethan <ethanjohn.montejo@cit.edu>
2026-03-09 00:50:38 +0800

    Product constructor to require category assignment

--- src/market/Product.java
+++ src/market/Product.java
@@ -4,5 +4,9 @@ public class Product {
     private String name;
     private double price;
     private String category; 
-    
+    public Product(String name, double price, String category) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
 }
