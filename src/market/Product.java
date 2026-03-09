a4f174fcdfccd87331c51a4010086216a98212ad
Ethan <ethanjohn.montejo@cit.edu>
2026-03-09 19:46:34 +0800

    Added private attributes and a constructor

--- src/market/Product.java
+++ src/market/Product.java
@@ -1,5 +1,14 @@
 package market;
 
 public class Product {
-    
+    private String name;
+    private double price;
+    private String category; // e.g., "Food" or "Transport"
+
+    public Product(String name, double price, String category) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
 }
