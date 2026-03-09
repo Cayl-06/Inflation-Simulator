b2215a02b2d058ffd12d118e7c06921dd9cfcc9c
Ethan <ethanjohn.montejo@cit.edu>
2026-03-09 19:18:09 +0800

    Deleted codes in product/Restart

--- src/market/Product.java
+++ src/market/Product.java
@@ -1,12 +1,5 @@
 package market;
 
 public class Product {
-    private String name;
-    private double price;
-    private String category; 
-    public Product(String name, double price, String category) {
-        this.name = name;
-        this.price = price;
-        this.category = category;
-    }
+    
 }
