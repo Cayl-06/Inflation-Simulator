5ddd241eff8d78ce31586e116c201930f27320ce
Cayl Redublado <josephcayl@gmail.com>
2026-03-09 19:06:25 +0800

    getter functions

--- src/household/Household.java
+++ src/household/Household.java
@@ -14,5 +14,29 @@ public abstract class Household {
         this.totalExpenses = 0;
     }
 
+    // Spend money on a product
+    public void spend(double amount) throws Exception {
+        if (amount > budget) {
+            throw new Exception("Error: Not enough budget for this purchase.");
+        }
+
+        budget -= amount;
+        totalExpenses += amount;
+    }
+
+    // Add income if needed
+    public void addIncome(double amount) {
+        income += amount;
+        budget += amount;
+    }
+
+    public double getBudget() {
+        return budget;
+    }
+
+    public double getTotalExpenses() {
+        return totalExpenses;
+    }
+
     
 }
