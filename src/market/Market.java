020db051f5c9428fa697fbd893679788252ef7fb
Ethan <ethanjohn.montejo@cit.edu>
2026-03-09 19:53:19 +0800

    Sets up the initial items

--- src/market/Market.java
+++ src/market/Market.java
@@ -1,5 +1,24 @@
 package market;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.text.DecimalFormat; // Imported DecimalFormat
+
 public class Market {
+    private List<Product> products;
+
+    public Market() {
+        this.products = new ArrayList<>();
+        initializeDefaultProducts();
+    }
+
+    // Sets up the initial items for Day 1
+    private void initializeDefaultProducts() {
+        products.add(new Product("Rice", 40.0, "Food"));
+        products.add(new Product("Meat", 150.0, "Food"));
+        products.add(new Product("Vegetables", 60.0, "Food"));
+        products.add(new Product("Transportation", 20.0, "Transport"));
+    }
+
     
 }
